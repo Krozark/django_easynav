126e14f2241cb0e37f0cde38ec998707bb07a7df
Krozark <maxime.barbier1991@gmail.com>
2012-04-25 13:31:07 -0400

    setup

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-easynav',
+    name='easynav',
     version='0.1.0',
     description='a module that allows you to manage the navigation, but also create generic pages stored entirely in the database for Django',
     author='Maxime Barbier',
     author_email='maxime.barbier1991@gmail.com',
     url='https://github.com/Krozark/django_easynav',
     download_url='',
-    packages=find_packages(),
+    #packages=find_packages(),
     include_package_data=True,
     #package_data = ('easynav': [
     #    ])
-    packages = ['textile','gblocks']
+    packages = ['textile','gblocks'],
     zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
